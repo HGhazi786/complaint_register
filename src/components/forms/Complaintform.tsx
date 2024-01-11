e83a4acd0fe1855a20068c5e660ce2f3c6e7ac11
hghazi786 <hmujtaba.ee41ceme@ee.ceme.edu.pk>
2024-01-11 08:30:43 +0500

    complaint form error resolved

--- src/components/forms/Complaintform.tsx
+++ src/components/forms/Complaintform.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
-function ProposalForm() {
+interface mid{
+  _id:number
+}
+
+function ComplaintForm(props:mid) {
   const {
     register,
     handleSubmit,
@@ -11,97 +15,49 @@ function ProposalForm() {
   } = useForm();
 
   const onSubmit = async (data: any) => {
-    const dateofInstallation = new Date(data.date_of_installation);
-
-    const currentDate = new Date();
-    const timediff = currentDate.getTime() - dateofInstallation.getTime();
-    const millisecondsInOneYear = 365 * 24 * 60 * 60 * 1000;
-    const isGreaterThan = timediff > millisecondsInOneYear;
-    console.log(isGreaterThan);
-    const inv = isGreaterThan;
-    // Handle form submission here
     const apiPost = async () => {
-      const response = await fetch("/api/user/", {
+      const response = await fetch("/api/complaints/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         cache: "no-cache",
         body: JSON.stringify({
-          username: data.username,
-          email: data.email,
-          contact_details: data.contact_details,
-          system_size: data.system_size,
-          date_of_installation: data.date_of_installation,
-          invoice_required: inv,
+          user_id: props._id,
+          title: data.title,
+          description: data.description,
         }),
       });
     };
     toast.promise(apiPost(), {
       loading: "Registering...",
-      success: "User registered successfully",
-      error: "Failed to registered User",
+      success: "Complaint registered successfully",
+      error: "Failed to registered Complaint",
     });
   };
 
   return (
-    <form
-      className="p-10 rounded-3xl glassmorphism shadow-2xl"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="sm:flex md:flex xl:flex lg:flex justify-between space-x-2">
-        <div className="block font-medium">
-          <label>User Name:</label>
-          <input
-            type="text"
-            id="username"
-            className="mt-1 p-1 font-sans focus:ring-0 focus:outline-none focus:ring-none block w-full text-brown bg-transparent border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
-            {...register("username", { required: true })}
-          />
-        </div>
-        <div className="block font-medium">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            className="mt-1 p-1 font-sans focus:outline-none focus:ring-none focus:ring-0 block w-full text-brown bg-transparent border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
-            {...register("email", { required: true })}
-          />
-        </div>
-      </div>
-      <div className="block font-medium">
-        <label>Contact Details:</label>
+    <form className="px-10" onSubmit={handleSubmit(onSubmit)}>
+    <div className="block font-medium">
+        <label>Title:</label>
         <input
           type="text"
-          id="contact_details"
-          className="mt-1 p-1 focus:outline-none focus:ring-none font-sans focus:ring-0 block w-full text-brown bg-transparent border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
-          {...register("contact_details", { required: true })}
+          id="title"
+          className="mt-1 p-1 text-gray-400 font-sans focus:ring-0 block w-full text-brown bg-transparent border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
+          {...register("title", { required: true })}
         />
       </div>
-      <div className="block font-medium">
-        <label>System Size:</label>
-        <input
-          type="number"
-          id="system_size"
-          className="mt-1 p-1 font-sans focus:ring-0 block w-full focus:outline-none focus:ring-none text-brown bg-transparent border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
-          {...register("system_size", { required: true })}
+      <div>
+        <label>Description:</label>
+        <textarea
+          id="description"
+          className="mt-1 p-1 text-gray-400 font-sans focus:ring-0 block w-full text-brown bg-transparent border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
+          {...register("description", { required: true })}
         />
       </div>
-      <div className="block font-medium">
-        <label>Date of Installation:</label>
-        <input
-          type="date"
-          id="date_of_installation"
-          className="mt-1 p-1 text-gray-400 font-sans focus:ring-0 block w-full text-brown bg-transparent focus:outline-none focus:ring-none border border-b-2 border-b-emerald-900 border-transparent shadow-sm"
-          {...register("date_of_installation", { required: true })}
-        />
-      </div>
-      <button
-        type="submit"
-        className="px-4 py-2 mt-5 bg-black text-white rounded-full"
-      >
+      <button type="submit" className="px-4 py-2 mt-5 bg-black text-white">
         Submit
       </button>
     </form>
   );
 }
 
-export default ProposalForm;
+export default ComplaintForm;
